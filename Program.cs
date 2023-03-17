e231c9e476ad4c6c6d4d7871067929671134ddd3
shishirb-MSFT <50385517+shishirb-MSFT@users.noreply.github.com>
2023-03-16 17:25:49 -0700

    Show usage of IDOManager::EnumDownloads (#3)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 
 // Sample code to showcase usage of Delivery Optimization client's COM API via C# Interop.
 // COM API is documented here: https://learn.microsoft.com/en-us/windows/win32/delivery_optimization/do-reference
@@ -15,65 +17,33 @@ namespace DODownloader
             // Full file streaming download: > .\DODownloader.exe --url $url
             // Partial file streaming download: > .\DODownloader.exe --url $url --ranges 10,65536,131072,65536
             // Full file download: > .\DODownloader.exe --url $url --output-file-path $env:TEMP\testfile.dat
+            // Enumerate existing downloads: .\DODownloader.exe --enumerate
             if (!Options.TryParseArgs(args, out Options options))
             {
-                Console.WriteLine("Usage: DODownloader.exe --url <url> [--output-file-path <path>]"
+                Console.WriteLine("Usage: DODownloader.exe --enumerate [--url <url>] | --url <url> [--output-file-path <path>]"
                     + " [--ranges <offset0,length0,offset1,length1,...>]");
                 return 1;
             }
 
-            options.SetRangesIfEmpty();
-
-            var factory = new DODownloadFactory(callerName: "DODownloader App");
-            var file = new DOFile(options.Url);
-
-            DODownload download = null;
-            SequentialStreamReceiver downloadDataSink = null;
-            try
+            if (options.Action == Options.Actions.EnumerateDownloads)
             {
-                if (options.IsStreamDownload)
-                {
-                    downloadDataSink = new SequentialStreamReceiver();
-                    download = factory.CreateDownloadWithStreamOutput(file, downloadDataSink);
-                }
-                else
-                {
-                    if (File.Exists(options.OutputFilePath))
-                    {
-                        File.Delete(options.OutputFilePath);
-                    }
-                    download = factory.CreateDownloadWithFileOutput(file, options.OutputFilePath);
-                }
-
-                download.SetForeground();
-                download.StartAndWaitUntilTransferred(options.DownloadRanges, completionTimeSecs: 60);
-                // Here, we can do something more like query stats from download.
-                // Then let DO client know that we are done with this download object.
-                download.Finalize2();
-
-                if (options.IsStreamDownload)
-                {
-                    Console.WriteLine($"Download completed, received {downloadDataSink.TotalBytesReceived} bytes via"
-                        + $" {downloadDataSink.TotalCallsReceived} stream write calls");
-                }
-                else
-                {
-                    var fileSize = new FileInfo(options.OutputFilePath).Length;
-                    Console.WriteLine($"Download completed, output file size {fileSize} bytes at {options.OutputFilePath}");
-                }
+                return ExecEnumeration(options.Url);
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"Download failed. Exception: hr: {ex.HResult:X}, {ex.Message}\n{ex.StackTrace}");
-                download?.Abort();
-                return 2;
+                return ExecDownload(options);
             }
-
-            return 0;
         }
 
-        struct Options
+        class Options
         {
+            public enum Actions
+            {
+                None,
+                EnumerateDownloads,
+            }
+
+            public Actions Action = Actions.None;
             public string Url;
             public string OutputFilePath;
             public DODownloadRanges DownloadRanges;
@@ -107,6 +77,7 @@ namespace DODownloader
                     if (args[i].Equals("--url"))
                     {
                         if ((i + 1) >= args.Length) return false;
+                        if (string.IsNullOrWhiteSpace(args[i + 1])) return false;
                         options.Url = args[++i];
                     }
                     else if (args[i].Equals("--output-file-path"))
@@ -127,13 +98,17 @@ namespace DODownloader
                             return false;
                         }
                     }
+                    else if (args[i].Equals("--enumerate"))
+                    {
+                        options.Action = Actions.EnumerateDownloads;
+                    }
                     else
                     {
                         Console.WriteLine($"Unknown cmdline option '{args[i]}'");
                         return false;
                     }
                 }
-                return !string.IsNullOrEmpty(options.Url);
+                return true;
             }
 
             private static DODownloadRanges ParseDownloadRanges(string arg)
@@ -147,6 +122,107 @@ namespace DODownloader
                 }
                 return new DODownloadRanges(offsetLengths);
             }
+        }
+
+        private static int ExecDownload(Options options)
+        {
+            options.SetRangesIfEmpty();
+
+            var factory = GetDODownloadFactory();
+            var file = new DOFile(options.Url);
+
+            DODownload download = null;
+            SequentialStreamReceiver downloadDataSink = null;
+            try
+            {
+                if (options.IsStreamDownload)
+                {
+                    downloadDataSink = new SequentialStreamReceiver();
+                    download = factory.CreateDownloadWithStreamOutput(file, downloadDataSink);
+                }
+                else
+                {
+                    if (File.Exists(options.OutputFilePath))
+                    {
+                        File.Delete(options.OutputFilePath);
+                    }
+                    download = factory.CreateDownloadWithFileOutput(file, options.OutputFilePath);
+                }
+
+                download.SetForeground();
+                download.StartAndWaitUntilTransferred(options.DownloadRanges, completionTimeSecs: 60);
+                // Here, we can do something more, like query stats from download.
+                // Then let DO client know that we are done with this download object.
+                download.Finalize2();
+
+                if (options.IsStreamDownload)
+                {
+                    Console.WriteLine($"Download completed, received {downloadDataSink.TotalBytesReceived} bytes via"
+                        + $" {downloadDataSink.TotalCallsReceived} stream write calls");
+                }
+                else
+                {
+                    var fileSize = new FileInfo(options.OutputFilePath).Length;
+                    Console.WriteLine($"Download completed, output file size {fileSize} bytes at {options.OutputFilePath}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Download failed. Exception: hr: {ex.HResult:X}, {ex.Message}\n{ex.StackTrace}");
+                download?.Abort();
+                return 2;
+            }
+            
+            return 0;
+        }
+
+        // Enumerate existing downloads with an optionally filtering by URL.
+        // Future: Could support other filtering on other properties.
+        private static int ExecEnumeration(string filterUrl)
+        {
+            var factory = GetDODownloadFactory();
+            List<IDODownload> downloads = (string.IsNullOrEmpty(filterUrl)) ?
+                factory.EnumerateDownloads() :
+                factory.EnumerateDownloads(DODownloadProperty.Uri, filterUrl);
+            Console.WriteLine($"Enumeration found {downloads.Count} download(s).");
+
+            uint i = 1;
+            foreach (var download in downloads)
+            {
+                download.GetProperty(DODownloadProperty.Id, out object id);
+                var url = (string)GetDownloadProperty(download, DODownloadProperty.Uri);
+                var outputFilePath = (string)GetDownloadProperty(download, DODownloadProperty.LocalPath);
+                Console.WriteLine($"{i++}: Download {id}");
+                if (url != null)
+                {
+                    Console.WriteLine($"\turl: {url}");
+                }
+                if (outputFilePath != null)
+                {
+                    Console.WriteLine($"\toutputFilePath: {outputFilePath}");
+                }
+                Console.WriteLine();
+            }
+            return 0;
+        }
+
+        private static object GetDownloadProperty(IDODownload download, DODownloadProperty downloadProperty)
+        {
+            try
+            {
+                download.GetProperty(downloadProperty, out object value);
+                return value;
+            }
+            catch (COMException ce)
+            {
+                Console.WriteLine($"Get property failed with {ce.HResult:x}");
+                return null;
+            }
+        }
+
+        private static DODownloadFactory GetDODownloadFactory()
+        {
+            return new DODownloadFactory(callerName: "DODownloader App");
         }
     }
 }
